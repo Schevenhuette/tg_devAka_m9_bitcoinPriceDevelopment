04f94b9acc10d81a183588754af01b4efb5ca1f9
Thomas Greff <t.greff@t-online.de>
2023-11-29 10:40:01 +0100

    comments deleted

--- script/script.js
+++ script/script.js
@@ -8,18 +8,13 @@ const API_KEY = '53G8MU0P10TSA54I';
 
 async function getBtcCourseData() {
     let dataBtcCourse = await fetch(url + API_KEY);
-    // console.log('dataBtcCourse = ', dataBtcCourse);
     let jsonBtcCourse = await dataBtcCourse.json();
-    // console.log('jsonBtcCourse = ', jsonBtcCourse);
     let arrBtcCourseKeys = Object.keys(jsonBtcCourse);
-    // console.log('arrBtcCourseKeys = ', arrBtcCourseKeys);
 
     if (arrBtcCourseKeys[0] === 'Information') {
-        // console.log('Das Tageslimit an möglichen Requests wurde erreicht!');
         getBtcCourseBackup(jsonCourseBackup);
         getMonthlyBtcCourseBackup();
     } else {
-        // console.log('Es wurden Daten von Alpha - Vantage geladen!');
         getBtcCourse(jsonBtcCourse);
     };
 }
@@ -38,18 +33,10 @@ async function getBtcCourseBackup(jsonBtcCourse) { // btc-eur exchange course fr
 
 async function getMonthlyBtcCourseBackup() { // historical monthly btc-eur exchange course from backup-data
     let jsonCourses = await jsonMonthlyCourseBackup;
-    // console.log('jsonCourses = ', jsonCourses);
     let arrCoursesKeys = Object.keys(jsonCourses);
-    // console.log('arrCoursesKeys = ', arrCoursesKeys);
-    // console.log('arrCourses Length  = ', Object.keys(jsonCourses).length);
     let arrCoursesTimeSeriesKeys = Object.keys(jsonCourses['Time Series (Digital Currency Monthly)']);
-    // console.log('arrCoursesTimeSeriesKeys = ', arrCoursesTimeSeriesKeys);
-    // console.log('arrCoursesTimeSeriesKeys Length = ', Object.keys(arrCoursesTimeSeriesKeys).length);
     let arrMonthly = [];
     for (let i = 0; i < arrCoursesTimeSeriesKeys.length; i++) {
-        // arrMonthly.push(jsonCourses['Time Series (Digital Currency Monthly)'][arrMonthly[i]]['4a. close (EUR)']);
-        // console.log(jsonCourses['Time Series (Digital Currency Monthly)'][arrCoursesTimeSeriesKeys[i]]['4a. close (EUR)']);
-        // .str(arrCoursesTimeSeriesKeys[i]));
         arrMonthly.push(Math.round(jsonCourses['Time Series (Digital Currency Monthly)'][arrCoursesTimeSeriesKeys[i]]['4a. close (EUR)'] * 100 ) / 100 );
         }
     console.log('arrMonthly = ', arrMonthly); 
@@ -60,9 +47,7 @@ async function getBtcCourse(jsonBtcCourse) { // actual btc-eur exchange course f
     let jsonCourse = jsonBtcCourse;
     console.log('jsonCourse = ', jsonCourse);
     let course = jsonCourse['Realtime Currency Exchange Rate']['5. Exchange Rate'];
-    // course = Math.round(jsonCourse['Realtime Currency Exchange Rate']['5. Exchange Rate']);
     course = Math.round(jsonCourse['Realtime Currency Exchange Rate']['5. Exchange Rate'] * 100) / 100;
-    // console.log('course = ', course, ' €');
     let output = document.getElementById('output').innerHTML = /*html*/ `
         <p>Das Tageslimit an möglichen Requests wurde noch nicht erreicht!<br>
         Der ausgegebene Wert wurde aktuell von <i><b>https://www.alphavantage.co/</b></i> gelesen.</p>
