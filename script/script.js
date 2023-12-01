fe01970f79b24aabb6ef8baa31eea58dcc28d080
Thomas Greff <t.greff@t-online.de>
2023-12-02 00:51:21 +0100

    added x achse werte, month[]

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@
 let url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=CNY&apikey=';
 let urlMonthly = 'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_MONTHLY&symbol=BTC&market=EUR&apikey=';
 const API_KEY = '53G8MU0P10TSA54I';
+let courses = [];
+let month = [];
 
 async function getBtcCourseData() { // fetch btc-data from alpha-vantage
     let dataBtcCourse = await fetch(url + API_KEY);
@@ -23,7 +25,6 @@ async function getBtcCourseData() { // fetch btc-data from alpha-vantage
 
 async function getCurrentBtcCourseBackup(jsonBtcCourseBackup) { // btc-eur exchange course from backup-data
     let jsonCourse = jsonBtcCourseBackup;
-    console.log('jsonCourse (backup) = ', jsonCourse);
     let course = Math.round(jsonCourse['Realtime Currency Exchange Rate']['5. Exchange Rate'] * 100) / 100;
     let output = document.getElementById('output').innerHTML = /*html*/ `
         <p>Das Tageslimit an möglichen Requests bei <i><b>Alpha-Vantage</b></i> wurde erreicht! <br>
@@ -35,25 +36,22 @@ async function getCurrentBtcCourseBackup(jsonBtcCourseBackup) { // btc-eur excha
 
 async function getMonthlyBtcCourseBackup(jsonMonthlyCourseBackup) { // historical monthly btc-eur exchange course from backup-data
     let jsonCourses = await jsonMonthlyCourseBackup;
-    console.log('jsonCourses (backup) = ', jsonCourses);
     let arrCoursesKeys = Object.keys(jsonCourses);
     let arrCoursesTimeSeriesKeys = Object.keys(jsonCourses['Time Series (Digital Currency Monthly)']);
-    let arrMonthlyCloseValues = [];
     for (let i = 0; i < arrCoursesTimeSeriesKeys.length; i++) {
-        arrMonthlyCloseValues.push(Math.round(jsonCourses['Time Series (Digital Currency Monthly)'][arrCoursesTimeSeriesKeys[i]]['4a. close (EUR)'] * 100 ) / 100 );
+        courses.push(Math.round(jsonCourses['Time Series (Digital Currency Monthly)'][arrCoursesTimeSeriesKeys[i]]['4a. close (EUR)'] * 100 ) / 100 );
+        month.push(arrCoursesTimeSeriesKeys[i]);
     }
-    console.log('arrMonthlyCloseValues (backup) = ', arrMonthlyCloseValues); 
     let output = document.getElementById('output').innerHTML += /*html*/ `
         <p>Das Tageslimit an möglichen Requests bei <i><b>Alpha-Vantage</b></i> wurde erreicht! <br>
             Diese ausgegebenen Werte kommen aus den <i><b>gespeicherten</b></i> Backup Daten. </p>
-        <b> ${arrMonthlyCloseValues} €</b>
+        <b> ${courses} €</b>
     `;
 }
 
 
 async function getCurrentBtcCourse(jsonBtcCourse) { // current btc-eur exchange course from alpha-vantage
     let jsonCourse = jsonBtcCourse;
-    console.log('jsonCourse (live) = ', jsonCourse);
     let course = jsonCourse['Realtime Currency Exchange Rate']['5. Exchange Rate'];
     course = Math.round(jsonCourse['Realtime Currency Exchange Rate']['5. Exchange Rate'] * 100) / 100;
     let output = document.getElementById('output').innerHTML = /*html*/ `
@@ -66,18 +64,16 @@ async function getCurrentBtcCourse(jsonBtcCourse) { // current btc-eur exchange 
 
 async function getMonthlyBtcCourse(jsonMonthlyBtcCourse) { // current monthly btc-eur exchange course from alpha-vantage
     let jsonMonthlyCourses = jsonMonthlyBtcCourse;
-    console.log('jsonMonthlyCourses (live) = ', jsonMonthlyCourses);
     let arrMonthlyCoursesKeys = Object.keys(jsonMonthlyCourses);
     let arrCoursesTimeSeriesKeys = Object.keys(jsonMonthlyCourses['Time Series (Digital Currency Monthly)']);
-    let arrMonthlyCloseValues = [];
     for (let i = 0; i < arrCoursesTimeSeriesKeys.length; i++) {
-        arrMonthlyCloseValues.push(Math.round(jsonMonthlyCourses['Time Series (Digital Currency Monthly)'][arrCoursesTimeSeriesKeys[i]]['4a. close (EUR)'] * 100 ) / 100 );
+        courses.push(Math.round(jsonMonthlyCourses['Time Series (Digital Currency Monthly)'][arrCoursesTimeSeriesKeys[i]]['4a. close (EUR)'] * 100 ) / 100 );
+        month.push(arrCoursesTimeSeriesKeys[i]);
     }
-    console.log('arrMonthlyCloseValues (live) = ', arrMonthlyCloseValues);
     let output = document.getElementById('output').innerHTML += /*html*/ `
         <p>Das Tageslimit an möglichen Requests wurde noch nicht erreicht!<br>
-        D1e ausgegebenen Werte wurden aktuell von <i><b>https://www.alphavantage.co/</b></i> gelesen.</p>
-        <b> ${arrMonthlyCloseValues} €</b>
+            D1e ausgegebenen Werte wurden aktuell von <i><b>https://www.alphavantage.co/</b></i> gelesen.</p>
+        <b> ${courses} €</b>
     `;
 }
 
